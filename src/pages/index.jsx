08562ba63437b7138974c669020341c87816882b
kananilai <kana.nilai4646@gmail.com>
2022-06-26 14:52:09 +0900

    ✨ useEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Head from "next/head";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
@@ -8,12 +8,22 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const foo = 1;
-
   const handleClick = useCallback((e) => {
     console.log(e.target.href);
     e.preventDefault();
     alert(foo);
   }, []);
+
+  //HomeComoponentがマウントするときに走る処理
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
