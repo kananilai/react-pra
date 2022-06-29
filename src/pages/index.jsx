cfcd9c34a986849695aa728781e77b39e167e43d
kananilai <kana.nilai4646@gmail.com>
2022-06-29 13:23:46 +0900

    ✨ useState 文字列と真偽値について

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,6 +8,8 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -15,7 +17,20 @@ export default function Home() {
     }
   }, [count]);
 
-  //HomeComoponentがマウントするときに走る処理
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    //引数には前回の値の状態が入る
+    setIsShow((isShow) => !isShow);
+    // return isShow ? false : true;
+  }, []);
+
+  // !HomeComoponentがマウントするときに走る処理
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
     return () => {
@@ -29,10 +44,14 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button href="/about" onClick={handleClick}>
         ボタン
       </button>
+      <button href="/about" onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
