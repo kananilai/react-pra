a456524ec23fa13c26547787c59c67125f8bd5f7
kananilai <kana.nilai4646@gmail.com>
2022-07-01 14:31:58 +0900

    🚸 APIを叩く際のローディング、エラー状態を追加

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,36 +1,17 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import Head from "next/head";
-import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/Header";
+import { Posts } from "src/components/Posts";
 import styles from "src/styles/Home.module.css";
 
-const Home = (props) => {
-  const [posts, setPosts] = useState([]);
-  const getPosts = useCallback(async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const json = await res.json();
-    setPosts(json);
-  }, []);
-
-  useEffect(() => {
-    // マウント時にgetPostsを呼び出す
-    getPosts();
-  }, [getPosts]);
-  console.log(posts);
-
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {posts.length > 0 ? (
-        <ol>
-          {posts.map((post) => {
-            return <li key={post.id}>{post.title}</li>;
-          })}
-        </ol>
-      ) : null}
+      <Posts />
     </div>
   );
 };
