67db9ceafb1941c79f8a30fe0a7c45c8655eae1d
kananilai <kana.nilai4646@gmail.com>
2022-07-08 13:37:04 +0900

    🎨 Layoutをコンポーネントを使ってまとめた

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
 import { Header } from "src/components/Header";
-import styles from "src/styles/Home.module.css";
 import { Post } from "src/components/Post";
 
 const PostsId = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Header />
       <Post />
     </div>
