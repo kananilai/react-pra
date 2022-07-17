537e830942f39e3acdca47e3f6708a21e0297cd6
kananilai <kana.nilai4646@gmail.com>
2022-07-17 11:21:42 +0900

    ✨APIを作成しISRを体験。コメントのみ実装

--- src/hooks/useComment.jsx
+++ src/hooks/useComment.jsx
@@ -1,13 +1,11 @@
 import { useRouter } from "next/router";
 import { API_URL } from "src/utils/const";
-import useSWR from "swr";
+import useSWRiImmutable from "swr/immutable";
 
 export const useComment = () => {
   const router = useRouter();
-  const { data, error } = useSWR(
-    router.query.id
-      ? `${API_URL}/comments/${router.query.id}`
-      : null
+  const { data, error } = useSWRiImmutable(
+    router.query.id ? `${API_URL}/comments/${router.query.id}` : null
   );
   return {
     data,
