bc1366cd08aef24b33b0a0fbda2fb29fd5bbc247
kananilai <kana.nilai4646@gmail.com>
2022-08-27 22:36:19 +0900

    useStateについて

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
 import Head from "next/head";
 import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
@@ -8,14 +7,16 @@ import styles from "src/styles/Home.module.css";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback((e) => {
-    console.log(count);
-    // if(count <10){
-      //前回の状態を受け取って新しい状態を返す
+  const handleClick = useCallback(
+    (e) => {
+      console.log(count);
       setCount((count) => count + 1);
-    // }
-  },[count]);
+    },
+    [count]
+  );
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -24,16 +25,32 @@ export default function Home() {
     };
   }, []);
 
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("５文字以内で入力してください");
+      return;
+    }
+    setText(e.target.value.trim());
+  }, []);
+
+  const handleDisplay = useCallback((e) => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
+      <button href="/about" onClick={handleDisplay}>
+        {isShow ? "非表示" : "表示"}
+      </button>
       <button href="/about" onClick={handleClick}>
         ボタン
       </button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
       <Footer />
     </div>
