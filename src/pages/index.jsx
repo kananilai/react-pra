a8a2c83eaa3a24fe9ec6d52553c7798ca5448502
kananilai <kana.nilai4646@gmail.com>
2022-08-29 20:17:14 +0900

    🎨 カスタムフックスでまとめてみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,52 +1,16 @@
 import Head from "next/head";
-import { useCallback, useEffect, useState } from "react";
 import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
 import styles from "src/styles/Home.module.css";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(
-    (e) => {
-      console.log(count);
-      setCount((prevCount) => prevCount + 1);
-    },
-    [count]
-  );
-
-  const handleDisplay = useCallback((e) => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("５文字以内で入力してください");
-      return;
-    }
-    setText(e.target.value.trim());
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleAdd, handleChange } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -70,7 +34,6 @@ export default function Home() {
           ))}
         </ul>
       </div>
-
       <Main page="index" />
       <Footer />
     </div>
