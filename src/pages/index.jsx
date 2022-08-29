b2cff056d18b4d7be8aa3a57dcf8afb08749d61d
kananilai <kana.nilai4646@gmail.com>
2022-08-29 16:40:35 +0900

    ✨ useStateで配列を扱った

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,7 @@ export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(
     (e) => {
@@ -21,6 +22,16 @@ export default function Home() {
   const handleDisplay = useCallback((e) => {
     setIsShow((prevIsShow) => !prevIsShow);
   }, []);
+
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素が既に存在します");
+        return prevArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -37,24 +48,29 @@ export default function Home() {
     setText(e.target.value.trim());
   }, []);
 
-  const handleDisplay = useCallback((e) => {
-    setIsShow((isShow) => !isShow);
-  }, []);
-
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
-      <button href="/about" onClick={handleDisplay}>
-        {isShow ? "非表示" : "表示"}
-      </button>
-      <button href="/about" onClick={handleClick}>
-        ボタン
-      </button>
-      <input type="text" value={text} onChange={handleChange} />
+      <div className={styles.wrapper}>
+        {isShow ? <h1>{count}</h1> : null}
+        <button href="/about" onClick={handleDisplay}>
+          {isShow ? "非表示" : "表示"}
+        </button>
+        <button href="/about" onClick={handleClick}>
+          ボタン
+        </button>
+        <input type="text" value={text} onChange={handleChange} />
+        <button onClick={handleAdd}>追加</button>
+        <ul>
+          {array.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+      </div>
+
       <Main page="index" />
       <Footer />
     </div>
