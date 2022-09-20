16d3432c01a4440491fd3b6ae3b2e51a071b3360
kananilai <kana.nilai4646@gmail.com>
2022-09-20 09:06:31 +0900

    ✨ APIを叩いてみた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,17 +1,36 @@
 import Head from "next/head";
-import { Footer } from "src/components/Footer";
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "src/components/Header";
-import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
 const Home = (props) => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = useCallback(async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const json = await res.json();
+    setPosts(json);
+    console.log(json);
+  }, []);
+
+  useEffect(() => {
+    getPosts();
+  }, [getPosts]);
+
+  console.log(posts);
   return (
-    <div className={styles.container}>
+    <div className={styles.wrapper}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      テスト
+      {posts.lenght > 0 ? (
+        <ol>
+          {posts.map((post) => {
+            return <li key={post.id}>{post.title}</li>;
+          })}
+        </ol>
+      ) : null}
     </div>
   );
 };
